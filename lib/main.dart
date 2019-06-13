656a3bb5b785d160b97b9c0c1a300ed5146b2448
ricardom6 <ricardosilvademenezes@gmail.com>
2019-06-13 17:43:41 -0300

    teste create git repository

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:aula401/ui/home.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  title: "Simple Layouts",
+  home: Home(),
+));
+
